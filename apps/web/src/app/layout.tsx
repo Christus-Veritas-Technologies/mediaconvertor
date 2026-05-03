482a11ab54fcb438e3a14e5538887b75da33d2a9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-05-03 13:52:12 +0200

    fix: add Google AdSense script and account information to layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,6 +14,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "MediaConvertor",
   description: "MediaConvertor",
+  other: {
+    "google-adsense-account": "ca-pub-6071419245494198",
+  },
 };
 
 export default function RootLayout({
@@ -23,6 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071419245494198"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         <Providers>
           <div className="grid h-svh grid-rows-[auto_1fr]">
